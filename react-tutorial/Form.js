e2905b0491f3cc9ac31e0315a7e18d30fda0df7f
wtharris89 <wtharris89@gmail.com>
2019-02-16 12:09:49 -0700

    added render method to Form.js and captured values from state

--- react-tutorial/Form.js
+++ react-tutorial/Form.js
@@ -11,4 +11,38 @@ class Form extends Component {
 
 		this.state = this.initialState;
 	}
+
+	handleChange = event => {
+		//runs every time a change is made to an input, passes through event, sets state of Form to have name and value inputs
+		const { name, value } = event.target;
+
+		this.setState({
+			[name]: value,
+		});
+	};
+
+	render() {
+		const { name, job } = this.state; // store our two properties from state
+
+		return (
+			<form>
+				<label>Name</label>
+				<input
+					type="text"
+					name="name"
+					value={name} // assign state value for name
+					onChange={this.handleChange}
+				/>
+				<label>Job</label>
+				<input
+					type="text"
+					name="job"
+					value={job} // assign state value for job
+					onChange={this.handleChange}
+				/>
+			</form>
+		);
+	}
 }
+
+export default Form;
