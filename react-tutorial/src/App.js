e838a0d51e939f7ae4ae8327803f688fd7fe73d2
wtharris89 <wtharris89@gmail.com>
2019-02-16 12:15:33 -0700

    created handleSubmit method on App and passed it through as a parameter on Form

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -16,6 +16,10 @@ class App extends Component {
 			}),
 		});
 	};
+
+	handleSubmit = character => {
+		this.setState({ characters: [...this.state.characters, character] });
+	};
 	render() {
 		const { characters } = this.state;
 
@@ -26,7 +30,7 @@ class App extends Component {
 					characterData={characters}
 					removeCharacter={this.removeCharacter}
 				/>
-				<Form />
+				<Form handleSubmit={this.handleSubmit} />
 			</div>
 		);
 	}
