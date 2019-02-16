1d2eaa6346c2df3e90e3f1b1b58c66056b0beccc
wtharris89 <wtharris89@gmail.com>
2019-02-16 12:12:16 -0700

    moved Form.js into src

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,33 +1,35 @@
 import React, { Component } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
-    state = {
-        characters: [],
-    };
+	state = {
+		characters: [],
+	};
 
-    removeCharacter = index => {
-        const { characters } = this.state;
+	removeCharacter = index => {
+		const { characters } = this.state;
 
-        this.setState({
-            characters: characters.filter((character, i) => {
-                return i !== index;
-            }),
-        });
-    };
-    render() {
-        const { characters } = this.state;
+		this.setState({
+			characters: characters.filter((character, i) => {
+				return i !== index;
+			}),
+		});
+	};
+	render() {
+		const { characters } = this.state;
 
-        return (
-            <div className="container">
-                <h1>React Tutorial</h1>
-                <Table
-                    characterData={characters}
-                    removeCharacter={this.removeCharacter}
-                />
-            </div>
-        );
-    }
+		return (
+			<div className="container">
+				<h1>React Tutorial</h1>
+				<Table
+					characterData={characters}
+					removeCharacter={this.removeCharacter}
+				/>
+				<Form />
+			</div>
+		);
+	}
 }
 
 export default App;
