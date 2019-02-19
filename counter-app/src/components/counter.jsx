c9221f5e6c925c8f95bd28e7a9f453a392c45774
wtharris89 <wtharris89@gmail.com>
2019-02-19 16:46:15 -0700

    added formatCount method and bootstrap classes to render method

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+	state = {
+		count: 0,
+	};
+
 	render() {
 		return (
 			<React.Fragment>
-				<h1>Hello World</h1>
-				<button>Increment</button>
+				<span className="badge badge-primary m-2">{this.formatCount()}</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
 			</React.Fragment>
 		);
+	}
+
+	formatCount() {
+		const { count } = this.state; // ES6 destructuring to simplify code
+		return count === 0 ? "Zero" : count;
 	}
 }
 
