0f88c1dc0c01a574c89ae9a0eb8e43330b766736
wtharris89 <wtharris89@gmail.com>
2019-02-21 10:30:28 -0700

    removed previous logic, was just used as an example

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,20 +6,24 @@ class Counter extends Component {
 		tags: ["tag1", "tag2", "tag3"],
 	};
 
-	renderTags() {
-		if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
+	render() {
 		return (
-			<ul>
-				{this.state.tags.map(tag => (
-					<li key={tag}>{tag}</li>
-				))}
-			</ul>
+			<React.Fragment>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
+			</React.Fragment>
 		);
 	}
 
-	render() {
-		return <React.Fragment>{this.renderTags()}</React.Fragment>;
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
+
+	formatCount() {
+		const { count } = this.state; // ES6 destructuring to simplify code
+		return count === 0 ? "Zero" : count;
 	}
 }
 
