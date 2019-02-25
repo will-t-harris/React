fa3cf61aed9a86bb99a781802887327d4def87d2
wtharris89 <wtharris89@gmail.com>
2019-02-25 13:29:27 -0700

    cleaning up old files

--- tic-tac-toe/src/index.jsx
+++ tic-tac-toe/src/index.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
 	render() {
-		return <button className="square">{/* TODO */}</button>;
+		return (
+			<button className="square" onClick={() => alert("click")}>
+				{this.props.value}
+			</button>
+		);
 	}
 }
 
 class Board extends React.Component {
 	renderSquare(i) {
-		return <Square />;
+		return <Square value={i} />;
 	}
 
 	render() {
